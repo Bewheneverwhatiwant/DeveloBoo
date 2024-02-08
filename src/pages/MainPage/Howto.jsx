b9594a452441a5d5af1040261f9d38fba6ac80e7
nayeong lee <lny021102@gmail.com>
2024-02-09 02:19:34 +0900

    [ADD] 사용방법 내용 추가

--- src/pages/MainPage/Howto.jsx
+++ src/pages/MainPage/Howto.jsx
@@ -13,7 +13,6 @@ border-radius: 20px;
 gap: 10px;
 
 width: 60%;
-height: 50vh;
 background-color: skyblue;
 `;
 
@@ -21,6 +20,7 @@ const Title = styled.h1`
 color: white;
 font-family: 'RIDIBatang';
 font-size: 15px;
+line-height: 20px;
 `;
 
 
@@ -28,7 +28,13 @@ export default function Component() {
 
     return (
         <Banner>
-            <Title>사용 방법 모달</Title>
+            <Title>1. 여러가지 아이템 중 원하는 것을 선택합니다.</Title>
+            <Title>1-1. 만약 말풍선을 추가한다면, 말풍선 클릭 시 텍스트를 입력할 수 있습니다.</Title>
+            <Title>1-3. 말풍선 안 텍스트는 7글자 이내로 작성해주세요.</Title>
+            <Title>2. 아래의 '미리보기' 버튼을 클릭하면, 내가 커스텀한 Boo를 확인할 수 있습니다.</Title>
+            <Title>2-1. 휴대전화의 너비에 따라 미리보기와 실제 Boo가 완벽하게 일치하지 않을 수도 있습니다.</Title>
+            <Title>3. 최하단의 '다운로드' 버튼을 눌러, 내가 만든 Boo를 저장할 수 있습니다.</Title>
+            <Title>4. 내가 만든 Boo를 SNS에 마구마구 자랑해주세요!</Title>
         </Banner>
     );
 }
