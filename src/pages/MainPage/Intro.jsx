d839cc0741fee1f632457594cc9344711ee918ae
nayeong lee <lny021102@gmail.com>
2024-02-09 00:31:35 +0900

    [ADD] 상단 배너 애니메이션 효과 추가

--- src/pages/MainPage/Intro.jsx
+++ src/pages/MainPage/Intro.jsx
@@ -1,12 +1,12 @@
-import React from 'react';
-import styled from 'styled-components';
-import CustomRow from '../../Components/Container/CustomRow';
-import CustomColumn from '../../Components/Container/CustomColumn';
+import React, { useState, useEffect } from 'react';
+import styled, { keyframes } from 'styled-components';
 
 const Banner = styled.div`
 display: flex;
+flex-direction: column;
 justify-content: center;
 align-items: center;
+padding: 10px;
 
 width: 100%;
 height: 200px;
@@ -21,12 +21,61 @@ font-family: 'RIDIBatang';
 font-size: 15px;
 `;
 
+const ListContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+`;
+
+const fadeIn = keyframes`
+  from {
+    opacity: 0;
+  }
+  to {
+    opacity: 1;
+  }
+`;
+
+const StyledImg = styled.img`
+  width: 80px;
+  height: 80px;
+  animation: ${fadeIn} 1s ease-out;
+`;
+
+const images = [
+    'Test_glass1.png',
+    'Test_jacket1.png',
+    'Test_glass2.png',
+    'icon_chat.png'
+];
+
+const ChatText = styled.a`
+color: white;
+font-family: 'RIDIBatang';
+font-size: 15px;
+line-height: 20px;
+`;
+
 export default function Test_buttons({ onSelect }) {
+    const [currentIndex, setCurrentIndex] = useState(0);
+
+    useEffect(() => {
+        const timer = setInterval(() => {
+            setCurrentIndex((prevIndex) => (prevIndex + 1) % images.length);
+        }, 2000);
+
+        return () => clearInterval(timer);
+    }, []);
+
     return (
         <Banner>
-            <Title>
-                여기는 인트로 페이지
-            </Title>
+            <ChatText>
+                여러가지 아이템을 조합해서<br />너의 대학생활을 표현해줘!
+            </ChatText>
+            <ListContainer>
+                <StyledImg src={images[currentIndex]} alt="슬라이드 이미지" />
+            </ListContainer>
         </Banner>
     );
-};
+}
